7c8289f56779e975ac3ab7c170cdb8e4724c83e1
valiclud <valicek@atlas.cz>
2023-08-07 12:33:39 +0200

    Some algorithm staff

---
+++ Hackerrank/algo/stringpangrams.py
@@ -0,0 +1,51 @@
+'''
+Created on 2. 8. 2023
+
+@author: valic
+'''
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pangrams' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def pangrams(s):
+    s = s.strip().lower()
+    chars = []
+    start = 97
+    for ch in s:
+        if ch != ' ':
+            i = ord(ch)
+            if (i in chars):
+                pass
+            else :
+                chars.append(i)
+    chars.sort()
+    if (len(chars) != 26):
+        return "not pangram"
+    
+    for k in chars :
+        if k != start:
+            return "not pangram"
+        start += 1
+    
+    return "pangram"
+
+if __name__ == '__main__':
+    #fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = pangrams(s)
+    print(result)
+
+    #fptr.write(result + '\n')
+
+    #fptr.close()
