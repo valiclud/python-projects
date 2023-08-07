755f6248e09b917df241b7260e93bd2df0962150
valiclud <valicek@atlas.cz>
2023-08-07 14:11:31 +0200

    Initial commit.

---
+++ Hackerrank/algo/minimaxsum.py
@@ -0,0 +1,31 @@
+'''
+Created on 1. 8. 2023
+
+@author: valic
+'''
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'miniMaxSum' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def miniMaxSum(arr):
+    arr.sort()
+    max = arr[len(arr) - 1]
+    count = 0
+    for i in arr :
+        if(i == max):
+            count += 1
+    print(count)
+
+if __name__ == '__main__':
+
+    arr = list(map(int, input().rstrip().split()))
+
+    miniMaxSum(arr)
